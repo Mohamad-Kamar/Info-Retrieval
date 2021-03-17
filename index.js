e2fbde5d0b0f547a49fce1a9842daab485878e53
Mohamad-Kamar <mohamad.kamar@saugo360.com>
2021-03-17 22:51:26 +0200

    Add basic implementation for the process so far
    .sfx and .stp

---
+++ index.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const sw = require('stopword')
+const stemmer = require('./stemmer').stemmer;
+function getText(fileName){
+    try {
+        const data = fs.readFileSync(`./sampleTexts/${fileName}.txt`, 'utf8')
+        // console.log(data)
+        return data;
+    }
+    catch (err) {
+        throw Error("Error at Reading the txt file")
+    }
+}
+
+function textToStpArray(text){
+    try{
+        const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const pureTextArray = text.split(/\W+/g);
+        const stopTextArray = sw.removeStopwords(pureTextArray);
+        return stopTextArray;
+    }
+    catch(error){
+        console.log(error);
+    }
+
+}
+
+function writeToStpFile(fileName,content){
+    try {
+        const data = fs.writeFileSync(`./sampleTexts/${fileName}.stp`, content,{ flag: 'w+' })
+        //file written successfully
+      } catch (err) {
+        console.error(err)
+      }    
+}
+function writeToSfxFile(fileName,content){
+    try {
+        const data = fs.writeFileSync(`./sampleTexts/${fileName}.sfx`, content,{ flag: 'w+' })
+        //file written successfully
+      } catch (err) {
+        console.error(err)
+      }    
+}
+function main(originalFilename){
+    let fileName = originalFilename;
+    
+    const text = getText(fileName);
+    const stpArray = textToStpArray(text);
+
+    const stpText = stpArray.join("\n");
+    writeToStpFile(fileName,stpText);
+
+    const stemmedWords = stpArray.map(word => stemmer(word)).filter(word => word).join('\n');
+    writeToSfxFile(fileName,stemmedWords);
+
+}
+main('myaw');
+
+
+
+
+
+
+// let s = `! @ # % ^ & * ( ) _ + | \ = - { 
+// } ] [ : ” ’ ; ? > < , . /`
+// let newS = s.trim().split(" ").join("");
+// console.log(newS);
+
