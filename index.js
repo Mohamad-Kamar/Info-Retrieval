572aa2f18b92b6213e85a1c2c88ff6b22b9156f6
Mohamad-Kamar <mohamad.kamar@saugo360.com>
2021-04-17 21:07:14 +0300

    Add a logger that uses moment, also add bootstrap link

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require("path");
 const app = express();
 const { getDirectories } = require("./utils");
 const port = process.env.PORT || 5000;
+const logger = require("./middleware/logger");
 
-app.get("/api/libraries", (req, res) =>
-  res.send(getDirectories("./Libraries"))
-);
+app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/libraries", (req, res) => res.json(getDirectories("Libraries")));
 app.listen(port, () =>
   console.log("\x1b[36m%s\x1b[0m", "Server started on port " + port)
 );
