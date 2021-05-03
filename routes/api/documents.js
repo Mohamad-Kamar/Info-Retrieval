550042507c5ea2bd2288d28fc6006760e39c4a68
Mohamad-Kamar <mohamad.kamar@saugo360.com>
2021-05-03 20:53:22 +0300

    current changes

--- routes/api/documents.js
+++ routes/api/documents.js
@@ -1,40 +1,34 @@
 const express = require("express");
 const path = require("path");
 
-const router = express.Router();
+const libraries = express.Router();
 const fs = require("fs");
-const { getDirectories, getFileNames } = require("./../../utils");
+const { getText } = require("./../../utils");
 
-let libraries = getDirectories("Libraries");
+libraries.get("/:docName", (req, res) => res.json(getText(req.params.libName)));
 
-router.get("/", (req, res) => res.json(libraries));
-
-router.get("/:lib", (req, res) => {
-    let docs;
-    try {
-        let requestedPath = path.join("Libraries", req.params.lib);
-        docs = getFileNames(requestedPath, "txt");
-        console.log(docs);
-        res.status(200).send(docs);
-    } catch {
-        res.status(400);
-        console.log("Library Not Found");
-    }
-});
-
-router.post("/", (req, res) => {
+libraries.post("/", (req, res) => {
     const dirName = req.body.name;
     const dirPath = path.join("Libraries", dirName);
     if (!libraries.find((folderName) => folderName == dirName)) {
         fs.mkdirSync(dirPath);
         libraries.push(dirName);
-        res.status(201).json(libraries);
+        res.json(libraries);
     } else {
         res.status(400).json({ message: "File Already Exists" });
     }
 });
 
-router.delete("/:lib", (req, res) => {
+libraries.get("/:lib", (req, res) => {
+    const found = libraries.find((lib) => lib == req.params.lib);
+    if (found) {
+        res.send(found);
+    } else {
+        res.status(400).send("No dir found");
+    }
+});
+
+libraries.delete("/:lib", (req, res) => {
     const found = libraries.find((lib) => lib == req.params.lib);
     if (found) {
         const dirPath = path.join("Libraries", found);
@@ -46,4 +40,6 @@ router.delete("/:lib", (req, res) => {
     }
 });
 
-module.exports = router;
+libraries.use("/:lib/document");
+
+module.exports = libraries;
